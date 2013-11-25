d73d1d945ac28e819ceded15e27dd4bcd34110ce
Nevill <nevill.dutt@gmail.com>
2013-11-25 09:57:17 +0800

    Add example.js

---
+++ example.js
@@ -0,0 +1,33 @@
+// Client code
+var ZongJi = require('./');
+
+var mysqlUrl = 'mysql://zongji:zongji@localhost';
+var listener = ZongJi.connect(mysqlUrl);
+
+// listener.setOption({
+//   logLevel: 'debug',
+//   retryLimit: 100,
+//   retryTimeout: 3 });
+
+listener.on('rotate', function(event) {
+  console.log('position:', event.position);
+  console.log('binlog name:', event.binlogName);
+});
+
+listener.on('format', function(event) {
+  event.dump();
+});
+
+listener.on('query', function(event) {
+  event.dump();
+});
+
+listener.on('xid', function(event) {
+  event.dump();
+});
+
+listener.on('unknown', function(event) {
+  event.dump();
+});
+
+listener.start();
