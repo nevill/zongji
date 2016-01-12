58b12bdb2351a04670d4e90a5d5ba40dafdd0951
Ben Green <ben@latenightsketches.com>
2016-01-12 13:03:55 -0800

    Run Travis CI Build using MySQL servers in Docker images
    * Created due to libaio1 and libaio-dev packages no longer being
      available on Travis build machine, see:
      https://travis-ci.org/nevill/zongji/builds/101945508
    * Tests now run also in MySQL server 5.7
    * Tests now run against Node.js 4.1 as well as 0.10

--- test/travis/runner.sh
+++ test/travis/runner.sh
@@ -1,8 +1,11 @@
-echo -e "\033[1;35m Run test on 5.1.73\033[0m"
-TEST_MYSQL_PORT=3351 npm test || exit $?
-echo -e "\033[1;35m Run test on 5.5.41\033[0m"
-TEST_MYSQL_PORT=3355 npm test || exit $?
-# echo -e "\033[1;35m Run test on 5.6.13\033[0m"
-# TEST_MYSQL_PORT=3456 npm test || exit $?
-echo -e "\033[1;35m Run test on 5.6.22\033[0m"
-TEST_MYSQL_PORT=3356 npm test || exit $?
+#!/bin/bash
+mysqlPorts=( 3351 3355 3356 3357 )
+for i in "${mysqlPorts[@]}"; do
+  while ! mysqladmin ping -h127.0.0.1 -P$i --silent; do
+    echo "$(date) - still trying $i"
+    sleep 1
+  done
+  echo "$(date) - connected successfully $i"
+  echo -e "\033[1;35m Running test on port $i\033[0m"
+  TEST_MYSQL_PORT=$i npm test || exit $?
+done
