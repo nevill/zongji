a821d7fab270a117ee84c43e4a7458245bdd3b83
Ben Green <ben@latenightsketches.com>
2016-01-25 05:27:17 +0000

    Getting close now
    * XXX: Blobs return as strings from binlog instead of buffers?
      This isn't new but I did not think it to be an issue before.
    * Type test cases now support larger rows that span multiple binlog
      packets
    * Nested arrays also now supported

--- test/types.js
+++ test/types.js
@@ -45,22 +45,24 @@ var defineTypeTest = function(name, fields, testRows, customTest, minVersion){
     var insertColumns = fields.map(function(field, index){
       return 'col' + index;
     }).join(', ');
-    var insertRows = testRows.map(function(row){
-      return '(' + row.map(function(field){
-        return field === null ? 'null' : field;
-      }).join(', ') + ')';
-    }).join(', ');
-
-    if(!minVersion || checkVersion(minVersion, conn.mysqlVersion)){
-      querySequence(conn.db, [
+    var testQueries = [
         'DROP TABLE IF EXISTS ' + conn.escId(testTable),
         'CREATE TABLE ' + conn.escId(testTable) + ' (' + fieldText + ')',
-        'SET @@session.time_zone = "+00:00"',
-        'INSERT INTO ' + conn.escId(testTable) +
-          ' (' + insertColumns + ') VALUES ' + insertRows,
+        'SET @@session.time_zone = "+00:00"']
+      .concat(testRows.map(function(row){
+        return 'INSERT INTO ' + conn.escId(testTable) +
+          ' (' + insertColumns + ') VALUES (' +
+            row.map(function(field){
+              return field === null ? 'null' : field;
+            }).join(', ') + ')';
+      }))
+      .concat([
         'SET @@session.time_zone = "SYSTEM"',
         'SELECT * FROM ' + conn.escId(testTable)
-      ], function(results){
+      ]);
+
+    if(!minVersion || checkVersion(minVersion, conn.mysqlVersion)){
+      querySequence(conn.db, testQueries, function(results){
         var selectResult = results[results.length - 1];
         var expectedWrite = {
           _type: 'WriteRows',
@@ -71,22 +73,6 @@ var defineTypeTest = function(name, fields, testRows, customTest, minVersion){
           }
         };
 
-        if(customTest){
-          expectedWrite._custom = customTest.bind(selectResult);
-        }else{
-          expectedWrite.rows = selectResult.map(function(row){
-            for(var field in row){
-              if(row.hasOwnProperty(field) &&
-                  row[field] instanceof Buffer &&
-                  name === 'blob'){
-                // Special case where blobs return as String instead of Buffer
-                row[field] = row[field].toString();
-              }
-            }
-            return row;
-          });
-        };
-
         expectEvents(test, conn.eventLog, [
           {
             _type: 'TableMap',
@@ -94,12 +80,24 @@ var defineTypeTest = function(name, fields, testRows, customTest, minVersion){
             schemaName: settings.database
           },
           expectedWrite
-        ], function(){
+        ], testRows.length, function(){
           test.equal(conn.errorLog.length, 0);
           conn.errorLog.length &&
             console.log('Type Test Error: ', name, conn.errorLog);
           if(conn.errorLog.length){
             throw conn.errorLog[0];
+          }
+          var binlogRows = conn.eventLog.reduce(function(prev, curr, index) {
+            if(curr.getTypeName() === 'WriteRows') {
+              prev = prev.concat(curr.rows);
+            }
+            return prev;
+          }, []);
+
+          if(customTest) {
+            customTest.bind(selectResult)(test, { rows: binlogRows });
+          } else {
+            test.deepEqual(selectResult, binlogRows);
           }
 
           test.done();
@@ -124,248 +122,267 @@ var checkVersion = function(check, actual){
 
 // Begin test case definitions
 
-// defineTypeTest('set', [
-//   'SET(' +
-//     '"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", ' +
-//     '"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ' +
-//     '"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2", "k2", ' +
-//     '"l2", "m2", "n2", "o2", "p2", "q2", "r2", "s2", "t2", "u2", "v2", ' +
-//     '"w2", "x2", "y2", "z2")',
-//   'SET("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")'
-// ], [
-//   ['"a,d"', '"a,d"'],
-//   ['"d,a,b"', '"d,a,b"'],
-//   ['"a,d,i,z2"', '"a,d,i,k,l,m,c"'],
-//   ['"a,j,d"', '"a,j,d"'],
-//   ['"d,a,p"', '"d,a,m"'],
-//   ['""', '""'],
-//   [null, null]
-// ]);
-// 
-// defineTypeTest('bit', [
-//   'BIT(64) NULL',
-//   'BIT(32) NULL',
-// ], [
-//   ["b'111'", "b'111'"],
-//   ["b'100000'", "b'100000'"],
-//   [
-//     // 64th position
-//     "b'1000000000000000000000000000000000000000000000000000000000000000'",
-//     // 32nd position
-//     "b'10000000000000000000000000000000'"
-//   ],
-//   [null, null]
-// ]);
-// 
-// defineTypeTest('enum', [
-//   'ENUM(' +
-//     '"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", ' +
-//     '"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ' +
-//     '"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2", "k2", ' +
-//     '"l2", "m2", "n2", "o2", "p2", "q2", "r2", "s2", "t2", "u2", "v2", ' +
-//     '"w2", "x2", "y2", "z2")',
-//   'ENUM("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")'
-// ], [
-//   ['"a"', '"b"'],
-//   ['"z2"', '"l"'],
-//   [null, null]
-// ]);
-// 
-// defineTypeTest('int_signed', [
-//   'INT SIGNED NULL',
-//   'BIGINT SIGNED NULL',
-//   'TINYINT SIGNED NULL',
-//   'SMALLINT SIGNED NULL',
-//   'MEDIUMINT SIGNED NULL'
-// ], [
-//   [2147483647, 9007199254740992, 127, 32767, 8388607],
-//   [-2147483648, -9007199254740992, -128, -32768, -8388608],
-//   [-2147483645, -9007199254740990, -126, -32766, -8388606],
-//   [-1, -1, -1, -1, -1],
-//   [123456, 100, 96, 300, 1000],
-//   [-123456, -100, -96, -300, -1000]
-// ]);
-// 
-// defineTypeTest('int_unsigned', [
-//   'INT UNSIGNED NULL',
-//   'BIGINT UNSIGNED NULL',
-//   'TINYINT UNSIGNED NULL',
-//   'SMALLINT UNSIGNED NULL',
-//   'MEDIUMINT UNSIGNED NULL'
-// ], [
-//   [4294967295, 9007199254740992, 255, 65535, 16777215],
-//   [1, 1, 1, 1, 1],
-//   [1, 8589934591, 1, 1, 1],
-//   [123456, 100, 96, 300, 1000]
-// ]);
-// 
-// defineTypeTest('double', [
-//   'DOUBLE NULL'
-// ], [
-//   [1.0], [-1.0], [123.456], [-13.47], [0.00005], [-0.00005],
-//   [8589934592.123], [-8589934592.123], [null]
-// ]);
-// 
-// defineTypeTest('float', [
-//   'FLOAT NULL'
-// ], [
-//   [1.0], [-1.0], [123.456], [-13.47], [3999.12]
-// ], function(test, event){
-//   // Ensure sum of differences is very low
-//   var diff = event.rows.reduce(function(prev, cur, index){
-//     return prev + Math.abs(cur.col0 - this[index].col0);
-//   }.bind(this), 0);
-//   test.ok(diff < 0.001);
-// });
-// 
-// defineTypeTest('decimal', [
-//   'DECIMAL(30, 10) NULL'
-// ], [
-//   [1.0], [-1.0], [123.456], [-13.47],
-//   [123456789.123], [-123456789.123], [null]
-// ]);
-// 
-// defineTypeTest('blob', [
-//   'BLOB NULL',
-//   'TINYBLOB NULL',
-//   'MEDIUMBLOB NULL',
-//   'LONGBLOB NULL'
-// ], [
-//   ['"something here"', '"tiny"', '"medium"', '"long"'],
-//   ['"nothing there"', '"small"', '"average"', '"huge"'],
-//   [null, null, null, null]
-// ]);
-// 
-// defineTypeTest('geometry', [
-//   'GEOMETRY',
-// ], [
-//   ['GeomFromText("POINT(1 1)")'],
-//   ['GeomFromText("POLYGON((0 0,10 0,10 10,0 10,0 0),(5 5,7 5,7 7,5 7, 5 5))")']
-// ]);
-// 
-// defineTypeTest('time_no_fraction', [
-//   'TIME NULL'
-// ], [
-//   ['"-00:00:01"'],
-//   ['"00:00:00"'],
-//   ['"00:07:00"'],
-//   ['"20:00:00"'],
-//   ['"19:00:00"'],
-//   ['"04:00:00"'],
-//   ['"-838:59:59"'],
-//   ['"838:59:59"'],
-//   ['"01:07:08"'],
-//   ['"01:27:28"'],
-//   ['"-01:07:08"'],
-//   ['"-01:27:28"'],
-// ]);
-// 
-// defineTypeTest('time_fraction', [
-//   'TIME(0) NULL',
-//   'TIME(1) NULL',
-//   'TIME(3) NULL',
-//   'TIME(6) NULL'
-// ], [
-//   ['"-00:00:01"', '"-00:00:01.1"', '"-00:00:01.002"', '"-00:00:01.123456"'],
-//   ['"00:00:00"',  '"00:00:00.2"',  '"00:00:00.123"',  '"-00:00:00.000001"'],
-//   ['"00:07:00"',  '"00:07:00.3"',  '"00:07:00.654"',  '"00:07:00.010203"'],
-//   ['"20:00:00"',  '"20:00:00.4"',  '"20:00:00.090"',  '"20:00:00.987654"'],
-//   ['"19:00:00"',  '"19:00:00.5"',  '"19:00:00.999"',  '"19:00:00.000001"'],
-//   ['"04:00:00"',  '"04:00:00.0"',  '"04:00:00.01"',  '"04:00:00.1"'],
-// ], '5.6.4');
-// 
-// defineTypeTest('datetime_no_fraction', [
-//   'DATETIME NULL'
-// ], [
-//   ['"1000-01-01 00:00:00"'],
-//   ['"9999-12-31 23:59:59"'],
-//   ['"2014-12-27 01:07:08"']
-// ]);
-// 
-// defineTypeTest('datetime_fraction', [
-//   'DATETIME(0) NULL',
-//   'DATETIME(1) NULL',
-//   'DATETIME(4) NULL',
-//   'DATETIME(6) NULL'
-// ], [
-//   ['"1000-01-01 00:00:00"', '"1000-01-01 00:00:00.5"',
-//    '"1000-01-01 00:00:00.9999"',  '"1000-01-01 00:00:00.123456"'],
-//   ['"9999-12-31 23:59:59"', '"9999-12-31 23:59:59.9"',
-//    '"9999-12-31 23:59:59.6543"',  '"9999-12-31 23:59:59.000001"'],
-//   ['"9999-12-31 23:59:59"', '"9999-12-31 23:59:59.1"',
-//    '"9999-12-31 23:59:59.1234"',  '"9999-12-31 23:59:59.4326"'  ],
-//   ['"2014-12-27 01:07:08"', '"2014-12-27 01:07:08.0"',
-//    '"2014-12-27 01:07:08.0001"',  '"2014-12-27 01:07:08.05"'    ]
-// ], '5.6.4');
-// 
-// defineTypeTest('timestamp_fractional', [
-//   'TIMESTAMP(3) NULL',
-// ], [
-//   ['"1970-01-01 00:00:01.123"'],
-//   ['"2038-01-18 03:14:07.900"'],
-//   ['"2014-12-27 01:07:08.001"'],
-// ], '5.6.4');
-// 
-// defineTypeTest('temporal_other', [
-//   'DATE NULL',
-//   'TIMESTAMP NULL',
-//   'YEAR NULL'
-// ], [
-//   ['"1000-01-01"', '"1970-01-01 00:00:01"', 1901],
-//   ['"9999-12-31"', '"2038-01-18 03:14:07"', 2155],
-//   ['"2014-12-27"', '"2014-12-27 01:07:08"', 2014]
-// ]);
-// 
-// defineTypeTest('string', [
-//   'VARCHAR(250) NULL',
-//   'CHAR(20) NULL',
-//   'BINARY(3) NULL',
-//   'VARBINARY(10) NULL'
-// ], [
-//   ['"something here"', '"tiny"', '"a"', '"binary"'],
-//   ['"nothing there"', '"small"', '"b"', '"test123"'],
-//   [null, null, null, null]
-// ]);
-// 
-// defineTypeTest('text', [
-//   'TINYTEXT NULL',
-//   'MEDIUMTEXT NULL',
-//   'LONGTEXT NULL',
-//   'TEXT NULL'
-// ], [
-//   ['"something here"', '"tiny"', '"a"', '"binary"'],
-//   ['"nothing there"', '"small"', '"b"', '"test123"'],
-//   [null, null, null, null]
-// ]);
-// 
-// defineTypeTest('datetime_then_decimal', [
-//   'DATETIME(3) NULL',
-//   'DECIMAL(30, 10) NULL'
-// ], [
-//   ['"1000-01-01 00:00:00.123"', 10.10],
-//   ['"9999-12-31 23:59:59.001"', -123.45],
-//   ['"2014-12-27 01:07:08.053"', 12345.123]
-// ], '5.6.4');
+defineTypeTest('set', [
+  'SET(' +
+    '"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", ' +
+    '"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ' +
+    '"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2", "k2", ' +
+    '"l2", "m2", "n2", "o2", "p2", "q2", "r2", "s2", "t2", "u2", "v2", ' +
+    '"w2", "x2", "y2", "z2")',
+  'SET("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")'
+], [
+  ['"a,d"', '"a,d"'],
+  ['"d,a,b"', '"d,a,b"'],
+  ['"a,d,i,z2"', '"a,d,i,k,l,m,c"'],
+  ['"a,j,d"', '"a,j,d"'],
+  ['"d,a,p"', '"d,a,m"'],
+  ['""', '""'],
+  [null, null]
+]);
+
+defineTypeTest('bit', [
+  'BIT(64) NULL',
+  'BIT(32) NULL',
+], [
+  ["b'111'", "b'111'"],
+  ["b'100000'", "b'100000'"],
+  [
+    // 64th position
+    "b'1000000000000000000000000000000000000000000000000000000000000000'",
+    // 32nd position
+    "b'10000000000000000000000000000000'"
+  ],
+  [null, null]
+]);
+
+defineTypeTest('enum', [
+  'ENUM(' +
+    '"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", ' +
+    '"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ' +
+    '"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2", "k2", ' +
+    '"l2", "m2", "n2", "o2", "p2", "q2", "r2", "s2", "t2", "u2", "v2", ' +
+    '"w2", "x2", "y2", "z2")',
+  'ENUM("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")'
+], [
+  ['"a"', '"b"'],
+  ['"z2"', '"l"'],
+  [null, null]
+]);
+
+defineTypeTest('int_signed', [
+  'INT SIGNED NULL',
+  'BIGINT SIGNED NULL',
+  'TINYINT SIGNED NULL',
+  'SMALLINT SIGNED NULL',
+  'MEDIUMINT SIGNED NULL'
+], [
+  [2147483647, 9007199254740992, 127, 32767, 8388607],
+  [-2147483648, -9007199254740992, -128, -32768, -8388608],
+  [-2147483645, -9007199254740990, -126, -32766, -8388606],
+  [-1, -1, -1, -1, -1],
+  [123456, 100, 96, 300, 1000],
+  [-123456, -100, -96, -300, -1000]
+]);
+
+defineTypeTest('int_unsigned', [
+  'INT UNSIGNED NULL',
+  'BIGINT UNSIGNED NULL',
+  'TINYINT UNSIGNED NULL',
+  'SMALLINT UNSIGNED NULL',
+  'MEDIUMINT UNSIGNED NULL'
+], [
+  [4294967295, 9007199254740992, 255, 65535, 16777215],
+  [1, 1, 1, 1, 1],
+  [1, 8589934591, 1, 1, 1],
+  [123456, 100, 96, 300, 1000]
+]);
+
+defineTypeTest('double', [
+  'DOUBLE NULL'
+], [
+  [1.0], [-1.0], [123.456], [-13.47], [0.00005], [-0.00005],
+  [8589934592.123], [-8589934592.123], [null]
+]);
+
+defineTypeTest('float', [
+  'FLOAT NULL'
+], [
+  [1.0], [-1.0], [123.456], [-13.47], [3999.12]
+], function(test, event){
+  // Ensure sum of differences is very low
+  var diff = event.rows.reduce(function(prev, cur, index){
+    return prev + Math.abs(cur.col0 - this[index].col0);
+  }.bind(this), 0);
+  test.ok(diff < 0.001);
+});
+
+defineTypeTest('decimal', [
+  'DECIMAL(30, 10) NULL'
+], [
+  [1.0], [-1.0], [123.456], [-13.47],
+  [123456789.123], [-123456789.123], [null]
+]);
+
+defineTypeTest('blob', [
+  'BLOB NULL',
+  'TINYBLOB NULL',
+  'MEDIUMBLOB NULL',
+  'LONGBLOB NULL'
+], [
+  ['"something here"', '"tiny"', '"medium"', '"long"'],
+  ['"nothing there"', '"small"', '"average"', '"huge"'],
+  [null, null, null, null]
+], function(test, event) {
+  // XXX: Blobs return as String from binlog instead of Buffer
+  event.rows.forEach(function(row, index) {
+    var expected = this[index];
+    for(var field in expected) {
+      if(expected.hasOwnProperty(field) && expected[field] instanceof Buffer) {
+        expected[field] = expected[field].toString();
+      }
+    }
+    test.deepEqual(expected, row);
+  }.bind(this));
+});
+
+defineTypeTest('geometry', [
+  'GEOMETRY',
+], [
+  ['GeomFromText("POINT(1 1)")'],
+  ['GeomFromText("POLYGON((0 0,10 0,10 10,0 10,0 0),(5 5,7 5,7 7,5 7, 5 5))")']
+]);
+
+defineTypeTest('time_no_fraction', [
+  'TIME NULL'
+], [
+  ['"-00:00:01"'],
+  ['"00:00:00"'],
+  ['"00:07:00"'],
+  ['"20:00:00"'],
+  ['"19:00:00"'],
+  ['"04:00:00"'],
+  ['"-838:59:59"'],
+  ['"838:59:59"'],
+  ['"01:07:08"'],
+  ['"01:27:28"'],
+  ['"-01:07:08"'],
+  ['"-01:27:28"'],
+]);
+
+defineTypeTest('time_fraction', [
+  'TIME(0) NULL',
+  'TIME(1) NULL',
+  'TIME(3) NULL',
+  'TIME(6) NULL'
+], [
+  ['"-00:00:01"', '"-00:00:01.1"', '"-00:00:01.002"', '"-00:00:01.123456"'],
+  ['"00:00:00"',  '"00:00:00.2"',  '"00:00:00.123"',  '"-00:00:00.000001"'],
+  ['"00:07:00"',  '"00:07:00.3"',  '"00:07:00.654"',  '"00:07:00.010203"'],
+  ['"20:00:00"',  '"20:00:00.4"',  '"20:00:00.090"',  '"20:00:00.987654"'],
+  ['"19:00:00"',  '"19:00:00.5"',  '"19:00:00.999"',  '"19:00:00.000001"'],
+  ['"04:00:00"',  '"04:00:00.0"',  '"04:00:00.01"',  '"04:00:00.1"'],
+], '5.6.4');
+
+defineTypeTest('datetime_no_fraction', [
+  'DATETIME NULL'
+], [
+  ['"1000-01-01 00:00:00"'],
+  ['"9999-12-31 23:59:59"'],
+  ['"2014-12-27 01:07:08"']
+]);
+
+defineTypeTest('datetime_fraction', [
+  'DATETIME(0) NULL',
+  'DATETIME(1) NULL',
+  'DATETIME(4) NULL',
+  'DATETIME(6) NULL'
+], [
+  ['"1000-01-01 00:00:00"', '"1000-01-01 00:00:00.5"',
+   '"1000-01-01 00:00:00.9999"',  '"1000-01-01 00:00:00.123456"'],
+  ['"9999-12-31 23:59:59"', '"9999-12-31 23:59:59.9"',
+   '"9999-12-31 23:59:59.6543"',  '"9999-12-31 23:59:59.000001"'],
+  ['"9999-12-31 23:59:59"', '"9999-12-31 23:59:59.1"',
+   '"9999-12-31 23:59:59.1234"',  '"9999-12-31 23:59:59.4326"'  ],
+  ['"2014-12-27 01:07:08"', '"2014-12-27 01:07:08.0"',
+   '"2014-12-27 01:07:08.0001"',  '"2014-12-27 01:07:08.05"'    ]
+], '5.6.4');
+
+defineTypeTest('timestamp_fractional', [
+  'TIMESTAMP(3) NULL',
+], [
+  ['"1970-01-01 00:00:01.123"'],
+  ['"2038-01-18 03:14:07.900"'],
+  ['"2014-12-27 01:07:08.001"'],
+], '5.6.4');
+
+defineTypeTest('temporal_other', [
+  'DATE NULL',
+  'TIMESTAMP NULL',
+  'YEAR NULL'
+], [
+  ['"1000-01-01"', '"1970-01-01 00:00:01"', 1901],
+  ['"9999-12-31"', '"2038-01-18 03:14:07"', 2155],
+  ['"2014-12-27"', '"2014-12-27 01:07:08"', 2014]
+]);
+
+defineTypeTest('string', [
+  'VARCHAR(250) NULL',
+  'CHAR(20) NULL',
+  'BINARY(3) NULL',
+  'VARBINARY(10) NULL'
+], [
+  ['"something here"', '"tiny"', '"a"', '"binary"'],
+  ['"nothing there"', '"small"', '"b"', '"test123"'],
+  [null, null, null, null]
+]);
+
+defineTypeTest('text', [
+  'TINYTEXT NULL',
+  'MEDIUMTEXT NULL',
+  'LONGTEXT NULL',
+  'TEXT NULL'
+], [
+  ['"something here"', '"tiny"', '"a"', '"binary"'],
+  ['"nothing there"', '"small"', '"b"', '"test123"'],
+  [null, null, null, null]
+]);
+
+defineTypeTest('datetime_then_decimal', [
+  'DATETIME(3) NULL',
+  'DECIMAL(30, 10) NULL'
+], [
+  ['"1000-01-01 00:00:00.123"', 10.10],
+  ['"9999-12-31 23:59:59.001"', -123.45],
+  ['"2014-12-27 01:07:08.053"', 12345.123]
+], '5.6.4');
 
 defineTypeTest('json', [
   'JSON NULL'
 ], [
-//   ['\'{"key1": "value1", "key2": "válue2", "keybá3": 34}\''],
+  ['\'{"key1": "value1", "key2": "válue2", "keybá3": 34}\''],
   ['\'{"key1": { "key2": "válue2", "keybá3": 34 } }\''],
-//   ['\'{ "key2": "válue2", "keybá3": 34 }\''],
-//   ['\'{"twobytelen": "' + strRepeat('a', 256) + '"}\''],
-//   ['\'{"twobytelen": "' + strRepeat('a', 257) + '"}\''],
-//   ['\'{"twobytelen": "' + strRepeat('a', 258) + '"}\''],
-//   ['\'{"twobytelen": "' + strRepeat('a', 16383) + '"}\''],
-//   ['\'{"threebytelen": "' + strRepeat('a', 16388) + '"}\''],
-//   ['\'{"key1": -10, "keyb": 34}\''],
-//   ['\'{"literaltest1": null, "literal2": true, "literal3": false}\''],
-//   ['\'["a", "b", 1]\'']
+  ['\'{"key1": { "key2": { "key2": "válue2", "keybá3": 34 }, "keybá3": 34 } }\''],
+  ['\'{ "key2": "válue2", "keybá3": 34 }\''],
+  ['\'{"twobytelen": "' + strRepeat('a', 256) + '"}\''],
+  ['\'{"twobytelen": "' + strRepeat('a', 257) + '"}\''],
+  ['\'{"twobytelen": "' + strRepeat('a', 258) + '"}\''],
+  ['\'{"twobytelen": "' + strRepeat('a', 7383) + '"}\''],
+  ['\'{"twobytelen": "' + strRepeat('a', 16383) + '"}\''],
+  ['\'{"threebytelen": "' + strRepeat('a', 16388) + '"}\''],
+  ['\'{"key1": -10, "keyb": 34}\''],
+  ['\'{"literaltest1": null, "literal2": true, "literal3": false}\''],
+  ['\'{"literaltest1": null, "stringafter": "heyos", "number": 35}\''],
+  ['\'["a", "b", 1]\''],
+  ['\'["a", [2, "b"], 1]\''],
+  ['\'["a", [2, ["b", 4, 54]], 1]\''],
+  ['\'"hello"\''],
+  ['\'null\''],
+  ['\'true\''],
+  ['\'false\''],
 ], function(test, event){
   // JSON from MySQL client has different whitespace than JSON.stringify
   // Therefore, parse and perform deep equality
   event.rows.forEach(function(row, index) {
-    console.log('this', this, row);
     var expected = JSON.parse(this[index].col0);
     var actual = JSON.parse(row.col0);
     test.deepEqual(expected, actual);
