0c08d4b2923325c8a1044b51f9ea1615c8f8739f
8secz-johndpope <50567111+8secz-johndpope@users.noreply.github.com>
2020-03-29 21:22:29 +1100

    Update example.js

--- example.js
+++ example.js
@@ -1,15 +1,23 @@
 // Client code
+// use docker-compose up to start numtel example db
 const ZongJi = require('./');
 
 const zongji = new ZongJi({
-  host     : 'localhost',
-  user     : 'zongji',
-  password : 'zongji',
-  // debug: true
+  host : 'localhost',
+  user : 'root',
+  password : 'numtel',
+  charset : 'utf8mb4_unicode_ci',
+  debug: true
 });
 
 zongji.on('binlog', function(evt) {
   evt.dump();
+  let database = evt.tableMap[evt.tableId].parentSchema;
+  let table = evt.tableMap[evt.tableId].tableName;
+  let columns = evt.tableMap[evt.tableId].columns;
+  console.log(database);
+  console.log(table);
+  console.log(columns);
 });
 
 zongji.start({
